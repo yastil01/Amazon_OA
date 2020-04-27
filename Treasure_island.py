d4fb41fd35b2bb08936a56644470edb8b21934ad
yastil01 <63788022+yastil01@users.noreply.github.com>
2020-04-26 20:15:59 -0700

    Update Treasure_island.py

--- Treasure_island.py
+++ Treasure_island.py
@@ -3,30 +3,30 @@ class Solution:
         if len(grid)==0 or len(grid[0])==0: return -1
         
         seen = {}
-        self.min_step_to_tresure = float('inf')
+        self.min_steps = float('inf')
         
-        def dfs(row,col,count):
+        def dfs(row,col,curr_steps):
             if row<0 or row>=len(grid) or col<0 or col>=len(grid[0])\
                 or grid[row][col]=='D' or (row,col) in seen:
                 return
 
             if grid[row][col] == 'X':
-                self.min_step_to_tresure = min(count, self.min_step_to_tresure)
+                self.min_steps = min(curr_steps, self.min_steps)
                 return
             
             seen[(row,col)] = 1
             
-            dfs(row,col+1,count+1)
-            dfs(row+1,col,count+1)
-            dfs(row,col-1,count+1)
-            dfs(row-1,col,count+1)
+            dfs(row,col+1,curr_steps+1)
+            dfs(row+1,col,curr_steps+1)
+            dfs(row,col-1,curr_steps+1)
+            dfs(row-1,col,curr_steps+1)
             
             del seen[(row,col)]
             
         dfs(0,0,0)
-        if self.min_step_to_tresure==float('inf'): return -1
-        return self.min_step_to_tresure
-        
+        if self.min_steps==float('inf'): return -1
+        return self.min_steps
+    
 obj = Solution()
 grid = [['O', 'O', 'O', 'O'], ['D', 'O', 'D', 'O'], ['O', 'O', 'O', 'O'], ['X', 'D', 'D', 'O']]
 grid1 = [['X', 'O', 'O', 'O'], ['D', 'O', 'D', 'O'], ['O', 'O', 'O', 'O'], ['D', 'D', 'D', 'O']]

61f9a2231bf07650a01a958ef81a5728b25b3166
yastil01 <63788022+yastil01@users.noreply.github.com>
2020-12-11 23:23:31 -0800

    Update cut_off_rank.py

--- cut_off_rank.py
+++ cut_off_rank.py
@@ -1,18 +1,11 @@
-def count_num_players_to_level_up(cut_off_rank, scores):
-    if cut_off_rank <= 0:
-        return 0
-
-    frequencies = [0 for i in range(100 + 1)]
-    for score in scores:
-        frequencies[score] += 1
-
-    ans = 0
-    current_rank = 1
-    for i in range(100, -1, -1):
-        if frequencies[i] == 0:
-            continue
-        ans += frequencies[i]
-        current_rank += frequencies[i]
-        if current_rank > cut_off_rank:
-            break
-    return ans
+def countLevelUpPlayers(cutOffRank, num, scores):                        
+    count = [0] * 101 # for storing count of 0 to 100 scores
+    for i in scores:
+        count[i] += 1
+    c = 0
+    for i in count[::-1]:
+        # Only if c >= cutoff, we should exit. Once we get into a score, we should take all count irrespective of cutoffrank.
+        if c >= cutOffRank:
+            break 
+        c += i
+    return c
